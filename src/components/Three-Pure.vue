8cdb214ee5d054f6f66680766a0f4f02f3a88a8a
大汉王朝 <jason.jiangyihan@gmail.com>
2022-11-03 18:03:29 +0800

    新增a-frame，替代torisjs作为3D渲染引擎

--- src/components/Three-Pure.vue
+++ src/components/Three-Pure.vue
@@ -1,7 +1,5 @@
 <template>
-    <!-- <a-entity camera look-controls position="500 500 500"></a-entity> -->
     <a-scene> 
-        <!-- <a-box color="red" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box> -->
         <a-entity id="rig"  position="0 0 0">
             <a-camera look-controls wasd-controls="false"></a-camera>
         </a-entity>
